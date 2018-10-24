94b1cd17cf8070f484af0f80653f9e09c6af7755
Jimmy Fruchterman <jdfruchterman@gmail.com>
2018-10-24 15:02:15 -0600

    changed date and gas price

--- JavaTheHardWay/GasolineReceipt.java
+++ JavaTheHardWay/GasolineReceipt.java
@@ -4,12 +4,12 @@ public class GasolineReceipt {
     System.out.println( "+                         +" );
     System.out.println( "+       CORNER STORE      +" );
     System.out.println( "+                         +" );
-    System.out.println( "+  2015-03-29  04:38PM    +" );
+    System.out.println( "+  2018-10-24  04:38PM    +" );
     System.out.println( "+                         +" );
     System.out.println( "+  Gallons:       10.870  +" );
-    System.out.println( "+  Price/gallon: $ 2.089  +" );
+    System.out.println( "+  Price/gallon: $ 3.089  +" );
     System.out.println( "+                         +" );
-    System.out.println( "+  Fuel total:  $ 22.71   +" );
+    System.out.println( "+  Fuel total:  $ 33.58   +" );
     System.out.println( "+                         +" );
     System.out.println( "+-------------------------+" );
   }
