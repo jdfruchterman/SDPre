929d0e2ec05c5761b7fa2aeb6c3fe157529e34ad
Jimmy Fruchterman <jdfruchterman@gmail.com>
2018-10-24 14:50:53 -0600

    complete drills

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg {
   public static void main( String[] args ) {
-    System.out.println( "I am determined to learn how to code." );
-    System.out.println( "Today's date is" );
+    System.out.println( "I, Jimmy Fruchterman, am determined to learn how to code." );
+    System.out.println( "Today's date is 10/24/2018." );
   }
 }
