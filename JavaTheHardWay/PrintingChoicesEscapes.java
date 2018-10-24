3d99d41aaa366f7b490a7134fdf98e65cd343125
Jimmy Fruchterman <jdfruchterman@gmail.com>
2018-10-24 16:51:04 -0600

    made it all print out on one line using escapes

--- JavaTheHardWay/PrintingChoicesEscapes.java
+++ JavaTheHardWay/PrintingChoicesEscapes.java
@@ -1,23 +1,6 @@
-public class PrintingChoices {
+public class PrintingChoicesEscapes {
   public static void main( String[] args ) {
-    System.out.println( "Alpha" );
-    System.out.println( "Bravo" );
+    System.out.println( "Alpha\nBravo\nCharlie\nDelta\n\nEchoFoxtrotGolf\nHotel\nIndia\n\nThis is a test.\nI am Learning Java the Hard Way!" );
 
-    System.out.println( "Charlie" );
-    System.out.println( "Delta" );
-    System.out.println();
-
-    System.out.print( "Echo" );
-    System.out.print( "Foxtrot" );
-
-    System.out.println( "Golf" );
-    System.out.print( "Hotel" );
-    System.out.println();
-    System.out.println( "India" );
-
-    System.out.println();
-    System.out.println( "This" + " " + "is" + " " + "a" + " test." );
-
-    System.out.println( "I" + " am" + " " + "Learning Java" + " " + "the Hard" + " Way!" );
   }
 }
