dd3dddde4dab9eec70be2629238fa794addd0edc
Jimmy Fruchterman <jdfruchterman@gmail.com>
2018-10-24 17:07:04 -0600

    finished objective1lab3.java

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3 {
+  public static void main( String[] args ) {
+    System.out.println( "Name: Jimmy Fruchterman" );
+    System.out.println( "Age: 32" );
+    System.out.println( "State: New York" );
+  }
+}
