bfdf41bcb6950f076e9b30759fce1a0b0c3d2f9d
Jimmy Fruchterman <jdfruchterman@gmail.com>
2018-10-24 19:14:24 -0600

    finished the drills but couldn't figure out #3 entirely

--- JavaTheHardWay/RudeQuestions.java
+++ JavaTheHardWay/RudeQuestions.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class RudeQuestions {
   public static void main( String[] args ) {
-    String name;
+    String name, party;
     int age;
     double weight, income;
 
@@ -24,7 +24,17 @@ public class RudeQuestions {
 
     System.out.print( "Hopefully that is " + income + " per hour" );
     System.out.println( " and not per year!" );
+
+    System.out.println( "What political party do you support, " + name + "?" );
+    party = keyboard.next();
+
+    System.out.println( "The " + party + " party, seriously? " + name + ", don't you know both parties are the same?" );
+
     System.out.print( "Well, thanks for answering my rude questions, ");
     System.out.println( name + "." );
   }
 }
+// 1. no because whole numbers can be doubles, you just can't have fractions in an integer
+// 2. no, because you can enter letters or numbers into a string
+// 3. easy to blow up all but the first question by entering letters or symbols, can't
+// figure out how to blow up the first question
