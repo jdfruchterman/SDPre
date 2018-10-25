155f049698f176cbabb2f9a62d3eff62bd00578f
Jimmy Fruchterman <jdfruchterman@gmail.com>
2018-10-24 18:13:16 -0600

    finished objective 2 lab 2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,13 @@
+public class Objective2Lab2 {
+  public static void main( String[] args ) {
+    String name = "Charles Peterson";
+    int age = 47;
+    String state = "Arkansas";
+
+    name = "Jimmy Fruchterman";
+    age = 32;
+    state = "Colorado";
+
+    System.out.println( "Name: " + name + " Age: " + age + " State: " + state );
+  }
+}
